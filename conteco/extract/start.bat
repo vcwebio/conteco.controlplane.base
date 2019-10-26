fdc2b21346999531a768dbdaa6a5e89942803c44
hvb <herman@vcweb.co>
2019-10-26 23:15:10 +0000

    create conteco and modeco controlplane inheriting from base

--- conteco/extract/start.bat
+++ conteco/extract/start.bat
@@ -1,6 +1,7 @@
 @echo off
-
+set mode=%1
+if not %1==modeco set mode = conteco 
 REM echo Starting controlplane.base
 REM start cmd.exe @cmd /k "start-output-console.bat"
 
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v c:\conteco\pwd:/conteco/pwd ${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.${CONTECO_NAME} --interactive start windows
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v c:\conteco\pwd:/conteco/pwd ${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.%mode% --interactive start  %mode% windows

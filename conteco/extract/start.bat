ef363bc415043e5a35ef79e6664c23ff0a860fd4
hvb <herman@vcweb.co>
2019-11-28 06:00:03 +0000

    added start option to set realm

--- conteco/extract/start.bat
+++ conteco/extract/start.bat
@@ -4,4 +4,4 @@ if not %1==modeco set mode = conteco
 REM echo Starting controlplane.base
 REM start cmd.exe @cmd /k "start-output-console.bat"
 
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v c:\conteco\pwd:/conteco/pwd ${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.%mode% --interactive start  %mode% windows
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v c:\conteco\pwd:/conteco/pwd ${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.%mode% --interactive start  %mode% windows %2
